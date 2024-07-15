afc23cf149c11e11560b0efd9dbb35ad9632af30
Siaw A. Nicholas <siawnic.dev@gmail.com>
2024-07-15 22:10:33 +0000

    initial boilerplate

---
+++ src/app/layout.tsx
@@ -0,0 +1,24 @@
+import type {Metadata} from "next";
+import {Poppins} from "next/font/google";
+import "./globals.css";
+
+const poppins = Poppins({subsets: ["latin"], weight: ["400", "500", "600", "700"], variable: "--font-poppins"});
+
+export const metadata: Metadata = {
+    title: "The Best Study Guide",
+    description: "We provide law students with a centralized platform, streamlining access to key resources for efficient bar exam preparation",
+    keywords: ["law", "bar exam", "study", "guide", "lawyers", "students", "legal", "resources"],
+    robots: "follow, index",
+};
+
+export default function RootLayout({
+                                       children,
+                                   }: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <html lang="en">
+        <body className={poppins.className}>{children}</body>
+        </html>
+    );
+}
