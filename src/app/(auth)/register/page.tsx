9bc39bb03c818ddf1a1b63503290264ea05969c7
Siaw A. Nicholas <54719123+ayequill@users.noreply.github.com>
2024-07-18 15:31:35 +0000

    fix: fixed spacing on auth routes (#6)

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -2,7 +2,7 @@ import RegisterForm from "@components/forms/sign-up-form";
 
 export default function Register() {
   return (
-    <main className="max-w-[455px] h-[calc(100vh_-_80px)] w-full px-4 lg:px-8 py-2 sm:py-[2%]">
+    <main className="max-w-[455px] w-ful h-full px-4 lg:px-8 py-2 sm:py-[2%]">
       <div className={"v-stack gap-6 h-full"}>
         <div className={"v-stack"}>
           <h1 className="text-2xl font-semibold">Sign up</h1>
--- src/components/core/navbar.tsx
+++ src/components/core/navbar.tsx
@@ -5,8 +5,8 @@ import React from "react";
 
 export default function NavBar(): React.ReactElement {
     return (
-        <header className={'sm:h-[78px] h-[56px]'}>
-            <nav className={'flex items-center bg-white border-[0.5px] border-[#D3D5D6] py-3 sm:py-5 px-4 lg:px-16 xl:px-20 w-full fixed top-0'}>
+        <header className={'sm:h-[78px] h-[56px] z-10'}>
+            <nav className={'flex items-center bg-white border-[0.5px] border-[#D3D5D6] py-3 sm:py-5 px-4 lg:px-16 xl:px-20 w-full fixed top-0 z-20'}>
                 <div>
                     <Link href={'/'}>
                         <Image loading={'lazy'} className={'w-7 sm:w-9'} placeholder={'blur'} src={Logo} alt={'BSG logo'} width={36} height={36} />
