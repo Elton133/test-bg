f13028737ec54f0fac05e56438389033a12654e0
Siaw A. Nicholas <siawnic.dev@gmail.com>
2024-07-25 14:16:42 +0000

    added page for my learning

---
+++ src/app/(dashboard)/dashboard/my-learning/page.tsx
@@ -0,0 +1,41 @@
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@components/ui/tabs";
+import CourseCardGrid from "@components/courses/course-card-grid";
+
+export default function Page() {
+  return (
+    <section className={"py-6 px-4"}>
+      <div className={"v-stack"}>
+        <h1 className={"text-2xl font-semibold"}>My learning</h1>
+        <p className={"text-sm font-medium text-muted"}>
+          Hi Jane, this is where you left off.
+        </p>
+      </div>
+      <div className={"pt-6 pb-4"}>
+        <Tabs defaultValue="progress" className="w[400px]">
+          <TabsList>
+            <TabsTrigger value="progress">In progress</TabsTrigger>
+            <TabsTrigger value="completed">Completed</TabsTrigger>
+          </TabsList>
+          <TabsContent value="progress">
+            <div className={"py-4 grid grid-col-1 sm:grid-cols-2 lg:grid-cols-4  sm:flex-row sm:justify-center lg:justify-start gap-y-4 sm:gap-x-4"}>
+              <CourseCardGrid courseName={"Civil procedure"} progress={50} />
+              <CourseCardGrid courseName={"Legal system"} progress={70} />
+              <CourseCardGrid courseName={"Family law"} progress={20} />
+              <CourseCardGrid courseName={"Family law"} progress={20} />
+              <CourseCardGrid courseName={"Family law"} progress={20} />
+              <CourseCardGrid courseName={"Family law"} progress={20} />
+
+            </div>
+          </TabsContent>
+          <TabsContent value="completed">
+            <div className={"py-4 flex flex-col sm:flex-row flex-wrap gap-4"}>
+              <CourseCardGrid courseName={"Civil procedure"} progress={100} />
+              <CourseCardGrid courseName={"Legal system"} progress={100} />
+              <CourseCardGrid courseName={"Family law"} progress={100} />
+            </div>
+          </TabsContent>
+        </Tabs>
+      </div>
+    </section>
+  );
+}
---
+++ src/components/courses/course-card-grid.tsx
@@ -0,0 +1,62 @@
+import Image from "next/image";
+import { Progress } from "@components/ui/progress";
+import {cn} from "@/lib/utils";
+import {Check} from "lucide-react";
+
+interface CourseCardProps {
+  courseName?: string;
+  progress?: number;
+  imageUrl?: string;
+}
+
+export default function CourseCardGrid({
+  courseName,
+  progress,
+  imageUrl,
+}: CourseCardProps) {
+  return (
+    <div
+      className={
+        "v-stack p-3 sm:max-w-[280px] bg-white shadow-md w-full rounded-[18px] animate-once animate-flip-up animate-duration-300 animate-delay-200"
+      }
+    >
+      <div className={"v-stack stack-center"}>
+        <Image
+          src={
+            imageUrl ||
+            "https://res.cloudinary.com/dzpjlfcrq/image/upload/v1721780876/c0e407a535283456f382c1a9d2c0c822_w46jpj.png"
+          }
+          alt={"course cover"}
+          className={
+            "rounded-lg w-full max-h-[180px] object-center object-cover"
+          }
+          width={303}
+          height={180}
+        />
+      </div>
+      <div className={"v-stack justify-between w-full py-4 gap-2"}>
+        <div className={"flex justify-between w-full"}>
+          <p className={"text-xs sm:text-sm font-semibold"}>{courseName}</p>
+          {/*<ArrowRight className={"text-muted text-base cursor-pointer"} />*/}
+        </div>
+        <div className={"font-semibold text-xs sm:text-sm v-stack"}>
+          <div className={"flex items-center justify-between"}>
+            <p className={""}>
+              {`${progress || 0}% `}{" "}
+              <span className={"text-muted"}>complete</span>
+            </p>
+            {progress === 100 &&
+              <Check color={"#4CAF50"} size={16} strokeWidth={4} />
+            }
+          </div>
+          <Progress
+            value={progress || 0}
+            className={cn("h-2", {
+              "bg-primary": progress === 100,
+            })}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
