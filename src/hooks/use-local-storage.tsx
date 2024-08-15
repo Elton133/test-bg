e1a14c44e7135655dd434eac7b41192cf0fcc79c
Siaw A. Nicholas <siawnic.dev@gmail.com>
2024-08-15 12:55:10 +0000

    added a custom for easy storage operations

---
+++ src/hooks/use-local-storage.tsx
@@ -0,0 +1,27 @@
+"use client";
+import LocalStorage from "@/lib/local-storage";
+import { useState } from "react";
+
+export default function useLocalStorage<T>(key: string, initialValue: T) {
+  const storage = new LocalStorage<T>();
+
+  const [storedValue, setStoredValue] = useState<T>(() => {
+    try {
+       return storage.get(key) || initialValue;
+    } catch (error) {
+      return initialValue;
+    }
+  });
+
+  const setValue = (value: T | ((val: T) => T)) => {
+    try {
+      const valueToStore =
+        value instanceof Function ? value(storedValue) : value;
+      setStoredValue(valueToStore);
+      storage.save(key, valueToStore);
+    } catch (error) {
+    }
+  };
+
+  return [storedValue, setValue] as const;
+}
