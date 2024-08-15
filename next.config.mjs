339b32ba1c4f4e7e3c2eb638954c32651536cb36
Siaw A. Nicholas <siawnic.dev@gmail.com>
2024-08-15 11:47:26 +0000

    modified next config

--- next.config.mjs
+++ next.config.mjs
@@ -12,17 +12,50 @@ const nextConfig = {
       },
       {
         protocol: "https",
-        hostname: "bsg.nyc3.cdn.digitaloceanspaces.com"
+        hostname: "bsg.nyc3.cdn.digitaloceanspaces.com",
       },
       {
         protocol: "https",
-        hostname: "ui-avatars.com"
-      }
+        hostname: "ui-avatars.com",
+      },
     ],
   },
   experimental: {
     missingSuspenseWithCSRBailout: false,
   },
+  // webpack: (config, { webpack }) => {
+  //   config.module.rules.push({
+  //     test: /\.(pdf)$/,
+  //     type: "asset/resource",
+  //   })
+  //   config.experiments = {
+  //     ...config.experiments,
+  //     topLevelAwait: true,
+  //   };
+  //   config.externals.push({
+  //     canvas: "commonjs canvas",
+  //   });
+  //   config.plugins.push(
+  //     new webpack.ProvidePlugin({
+  //       Buffer: ["buffer", "Buffer"],
+  //       // process: "process/browser",
+  //     }),
+  //   );
+  //   return config;
+  // },
+
+  // webpack: (config, options) => {
+  // config.module.rules.push({
+  //     test: /\.pdf$/,
+  //     use: {
+  //       loader: 'file-loader',
+  //       options: {
+  //         name: '[path][name].[ext]',
+  //       },
+  //     },
+  //   });
+  //   return config;
+  // },
 };
 
 export default nextConfig;
