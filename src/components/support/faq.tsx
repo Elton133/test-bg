94e655271371e6fb894a3eac9aa9064110c7620f
Mahama Hamida <mahamahamida@gmail.com>
2024-08-19 23:39:31 +0000

    refactor: Update FAQ component styles for better readability and consistency

--- src/components/support/faq.tsx
+++ src/components/support/faq.tsx
@@ -23,13 +23,13 @@ export default function Faq() {
     });
   };
   return (
-    <div className={"v-stack gap-4 w-full"}>
+    <div className={"v-stack gap-2 w-full"}>
       {faq.map((_, index) => (
         <Collapsible
           open={openStates[index]}
           onOpenChange={() => toggleState(index)}
           key={index}
-          className={"bg-gray-100 min-w-full p-3 rounded-[12px]"}
+          className={"bg-gray-100 min-w-full p-3 rounded-xl"}
         >
           <CollapsibleTrigger className={""}>
             <div className={"flex gap-2"}>
