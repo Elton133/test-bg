c9e8a05d36da4e1af7623eec7e8b4f9909336215
Mahama Hamida <mahamahamida@gmail.com>
2024-08-23 12:44:08 +0000

    refactor: Add lazy loading for images in NavBar component

--- src/components/core/navbar.tsx
+++ src/components/core/navbar.tsx
@@ -3,34 +3,49 @@
 import Image from "next/image";
 import Link from "next/link";
 import Logo from "@assets/logo.jpg";
-import React, {useState } from "react";
+import React, { useEffect, useState } from "react";
+import { usePathname } from "next/navigation";
 import { useSession } from "next-auth/react";
 import { NotificationBing, ShoppingCart } from "iconsax-react";
 import MobileNav from "@components/core/mobile-nav";
-import { IUser } from "@/types/user";
 import { signOut } from "next-auth/react";
-import {Cart, useCart} from "@/context/cart-context";
+import { Cart, useCart } from "@/context/cart-context";
 import { MenuButton } from "@components/ui/menu-button";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuTrigger,
-} from "@components/ui/dropdown-menu";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuTrigger } from "@components/ui/dropdown-menu";
 import { Button } from "@components/ui/button";
 import CartPanel from "@components/shop/cart-panel";
 
 export default function NavBar(): React.ReactElement {
+  const pathname = usePathname();
   const [open, setOpen] = React.useState(false);
   const [openCart, setOpenCart] = useState(false);
+  const [activeHash, setActiveHash] = useState("");
   const { data: session } = useSession();
-  const {dispatch, cart } = useCart();
+  const { dispatch, cart } = useCart();
+
+  // useEffect(() => {
+  //   const updateHash = () => {
+  //     setActiveHash(window.location.hash);
+  //   };
+
+  //   if (typeof window !== "undefined") {
+  //     updateHash();
+  //     window.addEventListener("hashchange", updateHash);
+  //   }
+
+  //   return () => {
+  //     if (typeof window !== "undefined") {
+  //       window.removeEventListener("hashchange", updateHash);
+  //     }
+  //   };
+  // }, []);
 
   const handleToggleSidebar = () => {
     setOpen(!open);
   };
 
   const handleLogout = async () => {
-    dispatch({ type: "CLEAR_CART", payload: {} as Cart});
+    dispatch({ type: "CLEAR_CART", payload: {} as Cart });
     await signOut();
   };
 
@@ -38,6 +53,12 @@ export default function NavBar(): React.ReactElement {
     setOpenCart(!openCart);
   };
 
+  // const handleActiveLink = (path: string, hash?: string) => {
+  //   if (hash) {
+  //     return activeHash === hash ? "text-yellow-400 border-b border-b-yellow-400" : "";
+  //   }
+  //   return pathname === path ? "text-yellow-400 border-b border-b-yellow-400" : "";
+  // };
 
   return (
     <header className={"sm:h-[78px] h-[56px] z-20"}>
@@ -56,7 +77,7 @@ export default function NavBar(): React.ReactElement {
               <MenuButton isOpen={open} onClick={handleToggleSidebar} />
             </div>
           )}
-          <Link href={"/"}>
+          <Link href="/">
             <Image
               loading={"lazy"}
               className={"w-7 sm:w-9"}
@@ -68,15 +89,56 @@ export default function NavBar(): React.ReactElement {
             />
           </Link>
         </div>
+        {pathname === "/" && (
+          <>
+          
+          <div>
+            <ul className="flex gap-4">
+              <li>
+                <a
+                  href="#about"
+                  className={`py-2 px-2 hover:text-yellow-400`}
+                >
+                  About
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#pricing"
+                  className={`py-2 px-2 hover:text-yellow-400`}
+                >
+                  Pricing
+                </a>
+              </li>
+              <li>
+                <a
+                  href="#faq"
+                  className={`py-2 px-2 hover:text-yellow-400`}
+                >
+                  FAQ
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div className="flex gap-2 text-sm font-semibold">
+            
+            <button className="h-10 px-4 py-2 text-primary bg-[#E6EBEA] rounded-lg hover:bg-[#DAE0E0]">Sign in</button>
+            <Button>Get started</Button>
+          </div>
+          </>
+        )}
         {session?.user && (
           <div className={"flex gap-2 items-center justify-start w-max"}>
             <DropdownMenu>
               <DropdownMenuTrigger>
-                {/*TODO: Fix next auth types*/}
                 <Image
-                    // @ts-ignore
-                  src={`${session?.user.image ? `${process.env.NEXT_PUBLIC_STORAGE_URL}/${session?.user.image}` : `https://ui-avatars.com/api/?name=${session?.user.firstName}+${session?.user.lastName}&background=063231&color=fff`}`}
-                    // @ts-ignore
+                  // @ts-ignore
+                  src={`${
+                    session?.user.image
+                      ? `${process.env.NEXT_PUBLIC_STORAGE_URL}/${session?.user.image}`
+                      : `https://ui-avatars.com/api/?name=${session?.user.firstName}+${session?.user.lastName}&background=063231&color=fff`
+                  }`}
+                  // @ts-ignore
                   alt={session.user.name as string}
                   width={28}
                   height={28}
@@ -86,11 +148,7 @@ export default function NavBar(): React.ReactElement {
                 />
               </DropdownMenuTrigger>
               <DropdownMenuContent>
-                <Button
-                  variant={"ghost"}
-                  className={"w-full"}
-                  onClick={handleLogout}
-                >
+                <Button variant={"ghost"} className={"w-full"} onClick={handleLogout}>
                   Logout
                 </Button>
               </DropdownMenuContent>
@@ -100,21 +158,15 @@ export default function NavBar(): React.ReactElement {
                 "text-muted cursor-pointer animate-fade animate-once animate-ease-linear"
               }
             />
-            <div className={'relative'}>
+            <div className={"relative"}>
               <ShoppingCart
                 onClick={handleToggleCart}
                 className={
                   "text-muted cursor-pointer animate-fade animate-once animate-ease-linear"
                 }
               />
-              <div className={'absolute -top-2 -right-2 bg-[#FF170A] text-white text-center text-xs font-semibold rounded-full w-4 h-4'}>
-                <span
-                    className={
-                    "text-xs"
-                    }
-                >
-                    {cart.length}
-                </span>
+              <div className={"absolute -top-2 -right-2 bg-[#FF170A] text-white text-center text-xs font-semibold rounded-full w-4 h-4"}>
+                <span className={"text-xs"}>{cart.length}</span>
               </div>
             </div>
           </div>
