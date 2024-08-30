347a863fd5f8057265470a870e54b097e9876fd1
Siaw A. Nicholas <siawnic.dev@gmail.com>
2024-08-30 16:17:02 +0000

    abstracted the course detail header

--- src/app/(dashboard)/dashboard/(course)/course/[slug]/page.tsx
+++ src/app/(dashboard)/dashboard/(course)/course/[slug]/page.tsx
@@ -2,13 +2,10 @@ import {
   getCourse,
   markNoteAsCompleted,
   markResourceAsCompleted,
-  resetCourseProgress,
 } from '@/actions/courses';
-import { Progress } from '@components/ui/progress';
-import { cn } from '@/lib/utils';
 import TopicCard from '@components/courses/topic-card';
-import ConfirmResetModal from '@components/courses/reset-course-modal';
 import { ICourseDetail } from '@/types/course';
+import CourseDetailHeader from '@components/courses/course-detail-header';
 
 export default async function CourseDetails({
   params,
@@ -16,6 +13,7 @@ export default async function CourseDetails({
   params: { slug: string };
 }) {
   const course: ICourseDetail = await getCourse(params.slug);
+
   return (
     <section className={'w-full h-full'}>
       <div
@@ -28,32 +26,7 @@ export default async function CourseDetails({
         className={'min-h-[180px] md:min-h-[380px] w-full'}
       />
       <div className={'py-8 md:py-12 px-4 sm:px-6 lg:px-16 min-h-full'}>
-        <div className={'v-stack'}>
-          <h1 className={'text-[28px] md:text-[40px] font-bold'}>
-            {course?.course?.title}
-          </h1>
-          <div
-            className={
-              'v-stack lg:h-stack justify-between items-start lg:items-center space-y-6'
-            }
-          >
-            <div className={'max-w-[400px] w-full space-y-2'}>
-              <p className={'font-semibold text-base'}>
-                {course?.progress_percentage?.toFixed(0)}% complete
-              </p>
-              <Progress
-                value={course?.progress_percentage}
-                className={cn('h-2', {
-                  // "bg-primary": progress === 100,
-                })}
-              />
-            </div>
-            <ConfirmResetModal
-              resetCourse={resetCourseProgress}
-              courseID={course?.course?.id}
-            />
-          </div>
-        </div>
+        <CourseDetailHeader course={course} />
         <div className={'py-6'}>
           <h2 className={'text-xl font-semibold'}>Topics</h2>
         </div>
