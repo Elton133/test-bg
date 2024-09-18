41747be0c81b74f5fed08971c9093cdeef7e1e98
Siaw A. Nicholas <siawnic.dev@gmail.com>
2024-09-18 23:48:22 +0000

    Add Clarity analytics script to layout component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,6 +9,7 @@ import { CircleCheck } from 'lucide-react';
 import { CartProvider } from '@/context/cart-context';
 import { getServerSession } from 'next-auth/next';
 import { authOptions } from '@/app/api/auth/[...nextauth]/options';
+import Script from 'next/script';
 
 const poppins = Poppins({
   subsets: ['latin'],
@@ -91,6 +92,17 @@ export default async function RootLayout({
             position={'top-center'}
           />
         </SessionWrapper>
+        <Script
+          type="text/javascript"
+          id="clarity-script"
+          strategy="afterInteractive"
+        >
+          {`(function(c,l,a,r,i,t,y){
+          c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+          t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+          y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+      })(window, document, "clarity", "script", "o5dq2fxipd")`}
+        </Script>
       </body>
     </html>
   );
