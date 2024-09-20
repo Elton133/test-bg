5da7bec9935210a41d3add30aa9f31b447af0ccb
Siaw A. Nicholas <siawnic.dev@gmail.com>
2024-09-20 23:09:59 +0000

    fix case briefs

--- src/components/courses/topic-card.tsx
+++ src/components/courses/topic-card.tsx
@@ -6,14 +6,24 @@ import {
   AccordionItem,
   AccordionTrigger,
 } from '@/components/ui/accordion';
-import { INoteStatuses, ITopic } from '@/types/course';
+import {
+  ICaseBrief,
+  INoteStatuses,
+  ITopic,
+  ITopicDetail,
+} from '@/types/course';
 import { Checkbox } from '@components/ui/checkbox';
 import Link from 'next/link';
 import { IResourcesCompleted } from '@/actions/courses';
 import SubscribeToCourseModal from '@components/courses/subscribe-to-course-modal';
+import { cn } from '@/lib/utils';
+import { useState } from 'react';
 
 interface TopicCardProps {
-  note: ITopic;
+  note: {
+    note: ITopic;
+    case_brief: ICaseBrief[];
+  };
   type?: string;
   noteStatuses: INoteStatuses;
   markNoteAsCompleted: (id: string) => void;
@@ -31,10 +41,17 @@ export default function TopicCard({
   markResourceAsCompleted,
   enrolled,
 }: TopicCardProps) {
+  const [openCaseBriefs, setOpenCaseBriefs] = useState(false);
+
+  const handleToggleCaseBriefs = () => {
+    setOpenCaseBriefs(!openCaseBriefs);
+  };
+
+  console.log(note?.case_brief);
   return (
     <Accordion type={'single'} collapsible>
       <AccordionItem
-        value={note?.slug}
+        value={note?.note?.slug}
         className={
           'bg-[#F1F2F2] px-4 md:px-8 py-2 rounded-lg animate-once animate-fade-up'
         }
@@ -44,13 +61,13 @@ export default function TopicCard({
             <Checkbox
               className={'absolute top-[20px] object-center'}
               onCheckedChange={() => {
-                markNoteAsCompleted(note?.id);
+                markNoteAsCompleted(note?.note?.id);
               }}
               checked={noteStatuses?.note_status}
             />
           )}
           <AccordionTrigger className={'text-left'}>
-            <p className={'text-base pl-6'}>{note?.noteTitle}</p>
+            <p className={'text-base pl-6'}>{note?.note?.noteTitle}</p>
           </AccordionTrigger>
         </div>
         <AccordionContent>
@@ -61,7 +78,7 @@ export default function TopicCard({
                   <Checkbox
                     className={'rounded-sm'}
                     onCheckedChange={() => {
-                      markResourceAsCompleted(note?.id, {
+                      markResourceAsCompleted(note?.note?.id, {
                         study_guide_completed:
                           !noteStatuses?.study_guide_status,
                       });
@@ -71,7 +88,7 @@ export default function TopicCard({
                 )}
                 {enrolled ? (
                   <Link
-                    href={`/dashboard/course/study-guide/${note?.slug}`}
+                    href={`/dashboard/course/study-guide/${note?.note?.slug}`}
                     className={
                       'text-base text-[#3A7FA8] hover:underline'
                     }
@@ -87,7 +104,7 @@ export default function TopicCard({
                   <Checkbox
                     className={'rounded-sm'}
                     onCheckedChange={() => {
-                      markResourceAsCompleted(note?.id, {
+                      markResourceAsCompleted(note?.note?.id, {
                         quiz_completed: !noteStatuses?.quiz_status,
                       });
                     }}
@@ -96,7 +113,7 @@ export default function TopicCard({
                 )}
                 {enrolled ? (
                   <Link
-                    href={`/dashboard/course/quiz/${note?.slug}`}
+                    href={`/dashboard/course/quiz/${note?.note?.slug}`}
                     className={
                       'text-base text-[#3A7FA8] hover:underline'
                     }
@@ -114,7 +131,7 @@ export default function TopicCard({
                   <Checkbox
                     className={'rounded-sm'}
                     onCheckedChange={() => {
-                      markResourceAsCompleted(note?.id, {
+                      markResourceAsCompleted(note?.note?.id, {
                         pqi_completed: !noteStatuses?.pqi_status,
                       });
                     }}
@@ -123,7 +140,7 @@ export default function TopicCard({
                 )}
                 {enrolled ? (
                   <Link
-                    href={`/dashboard/course/past-question/${note?.slug}`}
+                    href={`/dashboard/course/past-question/${note?.note?.slug}`}
                     className={
                       'text-base text-[#3A7FA8] hover:underline'
                     }
@@ -136,12 +153,16 @@ export default function TopicCard({
                   />
                 )}
               </div>
-              <div className={'h-stack w-full items-center'}>
+              <div
+                className={cn('h-stack w-full items-center', {
+                  'items-start': openCaseBriefs,
+                })}
+              >
                 {enrolled && (
                   <Checkbox
                     className={'rounded-sm'}
                     onCheckedChange={() => {
-                      markResourceAsCompleted(note?.id, {
+                      markResourceAsCompleted(note?.note?.id, {
                         case_brief_completed:
                           !noteStatuses?.case_brief_status,
                       });
@@ -150,9 +171,36 @@ export default function TopicCard({
                   />
                 )}
                 {enrolled ? (
-                  <p className={'text-base text-[#3A7FA8]'}>
-                    Case Briefs
-                  </p>
+                  <div>
+                    <button
+                      className={'text-base text-[#3A7FA8]'}
+                      onClick={handleToggleCaseBriefs}
+                    >
+                      Case Briefs
+                    </button>
+                    {openCaseBriefs && (
+                      <div className={'v-stack px-2 py-2'}>
+                        {note?.case_brief?.map((caseBrief) => (
+                          <Link
+                            href={`/dashboard/course/case-briefs/${caseBrief.slug}?topic=${note?.note?.slug}`}
+                            key={caseBrief.id}
+                            className={cn(
+                              'h-stack rounded-[5px] hover:underline animate-fade',
+                              {}
+                            )}
+                          >
+                            <p
+                              className={
+                                'text-sm truncate text-[#3A7FA8] hover:underline'
+                              }
+                            >
+                              {caseBrief.title}
+                            </p>
+                          </Link>
+                        ))}
+                      </div>
+                    )}
+                  </div>
                 ) : (
                   <SubscribeToCourseModal buttonText={'Case Briefs'} />
                 )}
