0a0dd2bda6fcbd188897853771579ee5f5814dd5
Siaw A. Nicholas <siawnic.dev@gmail.com>
2025-05-02 11:44:08 +0000

    Refactor announcements and skeleton components
    - Updated `getAnnouncements` to use 'no-store' cache policy for fresh data retrieval.
    - Removed unnecessary revalidation of the 'announcements' tag after marking an announcement as read.
    - Adjusted the Skeleton component's background color for improved UI consistency.

--- src/actions/announcements.ts
+++ src/actions/announcements.ts
@@ -1,13 +1,11 @@
 'use server';
-
-import { revalidateTag } from 'next/cache';
 import fetchWrapper from '@/lib/fetch-wrapper';
 
 export const getAnnouncements = async (): Promise<any> => {
   const url = `${process.env.NEXT_PUBLIC_API_URL}/api/announcements`;
   const response = await fetchWrapper<any>(
     url,
-    {},
+    { cache: 'no-store' },
     { tags: ['announcements'] }
   );
   if ('error' in response) {
@@ -19,16 +17,11 @@ export const getAnnouncements = async (): Promise<any> => {
 
 export const markAnnouncementAsRead = async (id: number) => {
   const url = `${process.env.NEXT_PUBLIC_API_URL}/api/announcements/${id}`;
-  const response = await fetchWrapper<any>(
-    url,
-    {
-      method: 'GET',
-    },
-    { tags: ['announcements'] }
-  );
+  const response = await fetchWrapper<any>(url, {
+    method: 'GET',
+  });
   if ('error' in response) {
     return response;
   }
-  revalidateTag('announcements');
   return response;
 };
