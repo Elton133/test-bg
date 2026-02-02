360d63fa081d85697d700905a1dc05fad9e29d0a
Elton133 <134627073+Elton133@users.noreply.github.com>
2026-02-02 18:32:41 +0000

    testtty3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,14 @@
 import FAQSection from '@/components/landing-page/faq';
-import Hero from '@components/landing-page/hero';
 import Explore from '@components/landing-page/explore';
 import ExploreTest from '@components/landing-page/explore-test';
 import Pricing from '@components/landing-page/pricing';
 import JoinUsSection from '@components/landing-page/join-us-section';
 import Footer from '@components/landing-page/footer';
+import dynamic from 'next/dynamic';
+
+const Hero = dynamic(() => import('@components/landing-page/hero'), {
+  ssr: false,
+});
 
 export default function Home() {
   return (
